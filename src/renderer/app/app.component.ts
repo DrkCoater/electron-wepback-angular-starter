a1aa78096c445e231f98fd7ba62a7fe83b0fabf2
Jeremy Lu <DrkCoater@gmail.com>
2019-08-13 14:44:28 -0700

    initial commit

---
+++ src/renderer/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component, ViewEncapsulation} from "@angular/core";
+import "./styles/app.scss";
+
+@Component({
+    selector: '#app',
+    templateUrl: './views/app.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {}
